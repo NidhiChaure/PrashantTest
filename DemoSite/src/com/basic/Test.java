290a0932b702f09565341e0b165fd390f98e3e42
NICKY <NICKY@DESKTOP-ITKI5LK>
2026-06-18 13:36:03 +0530

    Add Factorial program commit

--- DemoSite/src/com/basic/Test.java
+++ DemoSite/src/com/basic/Test.java
@@ -3,8 +3,15 @@ package com.basic;
 public class Test {
 
 	public static void main(String[] args) {
-		int a=20;
-
+		
+		int fact=1;
+		int num=5;
+		for(int i=1;i<=num;i++)
+		{
+			fact=fact*i;
+			
+		}
+System.out.println(fact);
 	}
 
 }
